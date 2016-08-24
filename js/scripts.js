fa81f86409b13b34c461d906f4758ca1ef6154be
sp <sampeppard@gmail.com>
2016-08-23 23:04:33 -0400

    make all variable names camel-case and fix form validation logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     event.preventDefault();
 
     // Business variables
-    var interest_php = 0;
-    var interest_css = 0;
-    var interest_java = 0;
-    var interest_ruby = 0;
-    var interest_csharp = 0;
+    var interestPhp = 0;
+    var interestCss = 0;
+    var interestJava = 0;
+    var interestRuby = 0;
+    var interestCsharp = 0;
 
     // UI variables
     var name = $("input#name").val();
@@ -22,105 +22,105 @@ $(document).ready(function() {
     if (name === "") {
       outcome.text("Please enter your name to recieve your results.");
     }
-    else if (question1 === undefined || question2 === undefined, question3 === undefined, question4 === undefined, question5 === undefined) {
+    else if (question1 === undefined || question2 === undefined || question3 === undefined || question4 === undefined || question5 === undefined) {
       outcome.text("Please answer all questions.");
     }
     else  {
 
       // question1
       if (question1 === "1") {
-        interest_java += 2;
-        interest_csharp += 2;
-        interest_ruby++;
-        interest_php++;
+        interestJava += 2;
+        interestCsharp += 2;
+        interestRuby++;
+        interestPhp++;
       }
       else if (question1 === "2") {
-        interest_css += 5;
+        interestCss += 5;
       }
       else if (question1 === "3") {
-        interest_ruby += 2;
-        interest_php++;
+        interestRuby += 2;
+        interestPhp++;
       }
       else {
-        interest_php += 2;
+        interestPhp += 2;
       }
 
       // question2
       if (question2 === "1") {
-        interest_css += 5;
+        interestCss += 5;
       }
       else {
-        interest_java += 3;
-        interest_csharp += 3;
-        interest_ruby += 3;
-        interest_php += 3;
+        interestJava += 3;
+        interestCsharp += 3;
+        interestRuby += 3;
+        interestPhp += 3;
       }
 
       // question3
       if (question3 === "1") {
-        interest_java += 2;
-        interest_csharp += 2;
-        interest_ruby++;
-        interest_php++;
+        interestJava += 2;
+        interestCsharp += 2;
+        interestRuby++;
+        interestPhp++;
       }
       else if (question3 === "2") {
-        interest_java++;
-        interest_csharp++;
-        interest_css++;
-        interest_ruby++;
-        interest_php++;
+        interestJava++;
+        interestCsharp++;
+        interestCss++;
+        interestRuby++;
+        interestPhp++;
       }
       else {
-        interest_php += 2;
-        interest_css += 2;
-        interest_ruby += 2;
+        interestPhp += 2;
+        interestCss += 2;
+        interestRuby += 2;
       }
 
       // question4
       if (question4 === "1") {
-        interest_php++;
-        interest_css++;
+        interestPhp++;
+        interestCss++;
       }
       else {
-        interest_java++;
-        interest_csharp++;
+        interestJava++;
+        interestCsharp++;
       }
 
       // question5
       if (question5 === "1") {
-        interest_css++;
+        interestCss++;
       }
       else if (question5 === "2") {
-        interest_php++;
+        interestPhp++;
       }
       else if (question5 === "3") {
-        interest_ruby++;
+        interestRuby++;
       }
       else if (question5 === "4") {
-        interest_java++;
+        interestJava++;
       }
       else {
-        interest_csharp++;
+        interestCsharp++;
       }
 
-      var result = [interest_php, interest_css, interest_java, interest_ruby, interest_csharp];
+      var result = [interestPhp, interestCss, interestJava, interestRuby, interestCsharp];
       result.sort();
       console.log(result);
       var suggestion = result[4];
 
-      if (interest_php === suggestion) {
+      if (interestPhp === suggestion) {
         outcome.text("Based on your answers " + name + ", you should take the PHP/Drupal track.");
       }
-      else if (interest_css === suggestion) {
+      else if (interestCss === suggestion) {
         outcome.text("Based on your answers " + name + ", you should take the CSS/Design track.");
       }
-      else if (interest_java === suggestion) {
+      else if (interestJava === suggestion) {
         outcome.text("Based on your answers " + name + ", you should take the Java/Android track.");
       }
-      else if (interest_ruby === suggestion) {
+      else if (interestRuby === suggestion) {
         outcome.text("Based on your answers " + name + ", you should take the Ruby/Rails track.");
       }
-      else if (interest_csharp === suggestion) {
+      else if (interestCsharp === suggestion) {
         outcome.text("Based on your answers " + name + ", you should take the C#/.NET track.");
       }
     }
